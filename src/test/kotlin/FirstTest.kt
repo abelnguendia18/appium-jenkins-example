2eb05bc7944196e54367c28ef4f35e482846a515
Abel Nguendia <56958137+abelnguendia18@users.noreply.github.com>
2025-05-03 10:51:46 +0200

    initial commit

---
+++ src/test/kotlin/FirstTest.kt
@@ -0,0 +1,43 @@
+import io.appium.java_client.AppiumDriver
+import io.appium.java_client.android.AndroidDriver
+import org.openqa.selenium.By
+import org.openqa.selenium.remote.DesiredCapabilities
+import org.testng.Assert
+import org.testng.annotations.AfterTest
+import org.testng.annotations.BeforeTest
+import org.testng.annotations.Test
+import java.net.URL
+
+class FirstTest {
+    lateinit var driver: AppiumDriver
+    private var desiredCapabilities = DesiredCapabilities()
+    private val appiumServerUrl = "http://localhost:4723/"
+    private val appPackage = "com.google.android.calculator"
+    private val platformName = "Android"
+    private val automationName = "UiAutomator2"
+    private val appActivity = "com.android.calculator2.Calculator"
+
+    @BeforeTest
+    fun setUp() {
+        desiredCapabilities.setCapability("platformName", platformName)
+        desiredCapabilities.setCapability("appium:automationName", automationName)
+        desiredCapabilities.setCapability("appium:appPackage", appPackage)
+        desiredCapabilities.setCapability("appium:appActivity", appActivity)
+        driver = AndroidDriver(URL(appiumServerUrl), desiredCapabilities)
+    }
+
+    @Test
+    fun `Simple multiplication gives correct result`() {
+        driver.findElement(By.id(DIGIT_5)).click()
+        driver.findElement(By.id(MULTIPLICATION)).click()
+        driver.findElement(By.id(DIGIT_8)).click()
+        driver.findElement(By.id(EQUAL)).click()
+        val result = driver.findElement(By.className("android.widget.TextView")).text
+        Assert.assertEquals(result, "40")
+    }
+
+    @AfterTest
+    fun tearDown() {
+        driver.quit()
+    }
+}
